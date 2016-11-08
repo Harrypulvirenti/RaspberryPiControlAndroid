e750e2757a7bbb933235b2a0f0b26772240d4a04
Harrypulvirenti <harrypulvirentidev@gmail.com>
2016-11-08 18:32:42 +0100

    Bugfix

--- app/src/main/java/com/hpdev/picontrol/RaspberryTCPClient.java
+++ app/src/main/java/com/hpdev/picontrol/RaspberryTCPClient.java
@@ -17,16 +17,16 @@ import java.util.ArrayList;
 
 public class RaspberryTCPClient extends AsyncTask {
 
-    public static final String TYPE_ADD_ROOM="room";
-    public static final String TYPE_ADD_USER="user";
-    public static final String TYPE_EXEC_COMMAND="command";
-    public static final String TYPE_UPDATE_REQUEST="update";
-    public static final String WAIT_MESSAGE="ok_send_me";
-    public static final String DONE_MESSAGE="done";
-    public static final String OPERATION_DONE="done";
-    public static final String OPERATION_FAIL="fail";
-    public static final String OPERATION_FAIL_NO_PIN="noPin";
-    public static final String ERROR="error";
+    public static final String TYPE_ADD_ROOM = "room";
+    public static final String TYPE_ADD_USER = "user";
+    public static final String TYPE_EXEC_COMMAND = "command";
+    public static final String TYPE_UPDATE_REQUEST = "update";
+    public static final String WAIT_MESSAGE = "ok_send_me";
+    public static final String DONE_MESSAGE = "done";
+    public static final String OPERATION_DONE = "done";
+    public static final String OPERATION_FAIL = "fail";
+    public static final String OPERATION_FAIL_NO_PIN = "noPin";
+    public static final String ERROR = "error";
 
     private String pi_ip;
     private Resources Res;
@@ -39,158 +39,151 @@ public class RaspberryTCPClient extends AsyncTask {
     private Command CommandResult;
 
     public RaspberryTCPClient(String pi_ip, Resources res, String type_request) {
-        this.pi_ip=pi_ip;
-        Res=res;
-        RequestType=type_request;
+        this.pi_ip = pi_ip;
+        Res = res;
+        RequestType = type_request;
     }
 
     public RaspberryTCPClient(String pi_ip, Resources res, String type_request, Command command) {
-        this.pi_ip=pi_ip;
-        Res=res;
-        RequestType=type_request;
-        CommandResult=command;
+        this.pi_ip = pi_ip;
+        Res = res;
+        RequestType = type_request;
+        CommandResult = command;
     }
 
-    public RaspberryTCPClient(String pi_ip, Resources res, String type_request,String roomName, int roomType) {
-        this.pi_ip=pi_ip;
-        Res=res;
-        RequestType=type_request;
-        this.roomName=roomName;
-        this.roomType=String.valueOf(roomType);
+    public RaspberryTCPClient(String pi_ip, Resources res, String type_request, String roomName, int roomType) {
+        this.pi_ip = pi_ip;
+        Res = res;
+        RequestType = type_request;
+        this.roomName = roomName;
+        this.roomType = String.valueOf(roomType);
     }
 
-    public RaspberryTCPClient(String pi_ip, Resources res, String type_request,String UserName, int UserType,String roomName) {
-        this.pi_ip=pi_ip;
-        Res=res;
-        RequestType=type_request;
-        this.UserName=UserName;
-        this.UserType=String.valueOf(UserType);
-        this.roomName=roomName;
+    public RaspberryTCPClient(String pi_ip, Resources res, String type_request, String UserName, int UserType, String roomName) {
+        this.pi_ip = pi_ip;
+        Res = res;
+        RequestType = type_request;
+        this.UserName = UserName;
+        this.UserType = String.valueOf(UserType);
+        this.roomName = roomName;
     }
 
     @Override
     protected Object doInBackground(Object[] params) {
         String serverResp = null;
-        BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
         Socket clientSocket = null;
         DataOutputStream outToServer = null;
         BufferedReader inFromServer = null;
-
-        Object Ret=ERROR;
-
+        Object Ret = ERROR;
         try {
             clientSocket = new Socket(this.pi_ip, Res.getInteger(R.integer.PortNumber));
+            clientSocket.setSoTimeout(1000);
             outToServer = new DataOutputStream(clientSocket.getOutputStream());
             inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            if(RequestType.equals(TYPE_ADD_ROOM)){
-
-                outToServer.writeBytes(TYPE_ADD_ROOM+"\n");
+            if (RequestType.equals(TYPE_ADD_ROOM)) {
+                outToServer.writeBytes(TYPE_ADD_ROOM + "\n");
                 serverResp = inFromServer.readLine();
-                if(serverResp.equals(WAIT_MESSAGE)){
-
-                    outToServer.writeBytes(roomName+"\n");
-                    outToServer.writeBytes(roomType+"\n");
+                if (serverResp.equals(WAIT_MESSAGE)) {
+                    outToServer.writeBytes(roomName + "\n");
+                    outToServer.writeBytes(roomType + "\n");
                     serverResp = inFromServer.readLine();
-                    if(serverResp.equals(DONE_MESSAGE)){
+                    if (serverResp.equals(DONE_MESSAGE)) {
                         clientSocket.close();
-                        Ret= OPERATION_DONE;
-                    }else{
+                        Ret = OPERATION_DONE;
+                    } else {
                         clientSocket.close();
-                        Ret= OPERATION_FAIL;
+                        Ret = OPERATION_FAIL;
                     }
                 }
-
             }
 
-            if(RequestType.equals(TYPE_UPDATE_REQUEST)){
+            if (RequestType.equals(TYPE_UPDATE_REQUEST)) {
 
-                outToServer.writeBytes(TYPE_UPDATE_REQUEST+"\n");
+                outToServer.writeBytes(TYPE_UPDATE_REQUEST + "\n");
                 serverResp = inFromServer.readLine();
-                String xml="";
-                if(serverResp.equals(WAIT_MESSAGE)){
+                String xml = "";
+                if (serverResp.equals(WAIT_MESSAGE)) {
 
-                    while (true){
+                    while (true) {
                         serverResp = inFromServer.readLine();
-                        if(!serverResp.equals(DONE_MESSAGE)){
-                            xml+=serverResp+"\n";
-                        }else{
-                            xml = xml.substring(0, xml.length()-1);
+                        if (!serverResp.equals(DONE_MESSAGE)) {
+                            xml += serverResp + "\n";
+                        } else {
+                            xml = xml.substring(0, xml.length() - 1);
                             break;
                         }
 
                     }
                 }
                 clientSocket.close();
-                Ret= xml;
+                Ret = xml;
 
             }
 
-            if(RequestType.equals(TYPE_ADD_USER)){
+            if (RequestType.equals(TYPE_ADD_USER)) {
 
-                outToServer.writeBytes(TYPE_ADD_USER+"\n");
+                outToServer.writeBytes(TYPE_ADD_USER + "\n");
                 serverResp = inFromServer.readLine();
-                if(serverResp.equals(WAIT_MESSAGE)){
-                    serverResp="";
-                    outToServer.writeBytes(UserType+"\n");
+                if (serverResp.equals(WAIT_MESSAGE)) {
+                    serverResp = "";
+                    outToServer.writeBytes(UserType + "\n");
                     serverResp = inFromServer.readLine();
-                    if(serverResp.equals(WAIT_MESSAGE)){
-                        outToServer.writeBytes(UserName+"\n");
-                        outToServer.writeBytes(roomName+"\n");
-                        ArrayList<XMLPin> users=new ArrayList<XMLPin>();
-                        while (true){
+                    if (serverResp.equals(WAIT_MESSAGE)) {
+                        outToServer.writeBytes(UserName + "\n");
+                        outToServer.writeBytes(roomName + "\n");
+                        ArrayList<XMLPin> users = new ArrayList<XMLPin>();
+                        while (true) {
                             serverResp = inFromServer.readLine();
-                            if(!serverResp.equals(DONE_MESSAGE)){
-                                users.add(new XMLPin(Integer.parseInt(serverResp),UserName,Integer.parseInt(UserType)));
-                            }else{
+                            if (!serverResp.equals(DONE_MESSAGE)) {
+                                users.add(new XMLPin(Integer.parseInt(serverResp), UserName, Integer.parseInt(UserType)));
+                            } else {
                                 clientSocket.close();
-                                Ret= users;
+                                Ret = users;
                             }
 
                         }
 
-                    }else{
+                    } else {
                         clientSocket.close();
-                        Ret= OPERATION_FAIL_NO_PIN;
+                        Ret = OPERATION_FAIL_NO_PIN;
                     }
                 }
             }
 
 
-            if(RequestType.equals(TYPE_EXEC_COMMAND)){
+            if (RequestType.equals(TYPE_EXEC_COMMAND)) {
 
-                outToServer.writeBytes(TYPE_EXEC_COMMAND+"\n");
+                outToServer.writeBytes(TYPE_EXEC_COMMAND + "\n");
                 serverResp = inFromServer.readLine();
-                if(serverResp.equals(WAIT_MESSAGE)){
+                if (serverResp.equals(WAIT_MESSAGE)) {
 
-                    outToServer.writeBytes(CommandResult.getRoomName()+"\n");
-                    outToServer.writeBytes(CommandResult.getUserName()+"\n");
-                    outToServer.writeBytes(CommandResult.getCommand()+"\n");
-                    String resp=inFromServer.readLine();
+                    outToServer.writeBytes(CommandResult.getRoomName() + "\n");
+                    outToServer.writeBytes(CommandResult.getUserName() + "\n");
+                    outToServer.writeBytes(CommandResult.getCommand() + "\n");
+                    String resp = inFromServer.readLine();
                     serverResp = inFromServer.readLine();
-                    if(serverResp.equals(DONE_MESSAGE)){
+                    if (serverResp.equals(DONE_MESSAGE)) {
                         clientSocket.close();
-                        Ret= resp;
-                    }else{
+                        Ret = resp;
+                    } else {
                         clientSocket.close();
-                        Ret= OPERATION_FAIL;
+                        Ret = OPERATION_FAIL;
                     }
 
 
-                }else{
+                } else {
                     clientSocket.close();
 
-                    Ret= OPERATION_FAIL;
+                    Ret = OPERATION_FAIL;
                 }
 
 
             }
-
 
 
         } catch (IOException e) {
 
-        }finally {
+        } finally {
             return Ret;
         }
 
